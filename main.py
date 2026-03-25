1f3c7a27660d115c294a79345b887d74f837eb26
“John <john@email.com>
2026-03-25 14:49:36 +0200

    hw

---
+++ main.py
@@ -0,0 +1,15 @@
+import poems
+import sign
+from poems import POEM_1
+from poems import POEM_2
+
+author = "Андрій Тарасович"
+print(sign.HOMEWORK_DONE.format(name=author))
+wishes = "Дізнатися більше інформації n/ вивчити пайтон"
+print(wishes)
+zirochka = "**************************************************************************"
+print(zirochka)
+print(POEM_1)
+print(zirochka)
+print(POEM_2)
+print(zirochka)
