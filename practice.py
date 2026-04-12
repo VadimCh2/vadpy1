9744035cbe11635c609522be3f29dfb7a0dc2653
“John <john@email.com>
2026-04-12 16:31:06 +0300

    hz

--- practice.py
+++ practice.py
@@ -1,28 +1,28 @@
-# my_hobbies = (
-#     "soccer tennis chess         kdghku e guegyery guoe"
-#     "rh guieriugi yergreg yertuygoiuerygiouryiougyrte"
-#     "iouygiu  rtgyrtiougyrtyirtyrtitrryiourtitrgti ut"
-# )
-# something_new = my_hobbies.split()
-# print(something_new)
-# my_list0a = []
-# my_list0b = ['apples', 'milk', 'bread']
-# my_list0c = [
-#     'apples',  # macintosh
-#     'milk',
-#     'bread',
-#     'milk',  # selyanske
-#     'bread',
-#     'milk',
-#     'bread',
-#     'milk',
-#     'bread',
-#     'bread',
-#     'bread',
-#     'milk',
-# ]
-# print(my_list0c)
-# my_list1 = ['soccer', 'tennis', 'chess']
+my_hobbies = (
+    "soccer tennis chess         kdghku e guegyery guoe"
+    "rh guieriugi yergreg yertuygoiuerygiouryiougyrte"
+    "iouygiu  rtgyrtiougyrtyirtyrtitrryiourtitrgti ut"
+)
+something_new = my_hobbies.split()
+print(something_new)
+my_list0a = []
+my_list0b = ['apples', 'milk', 'bread']
+my_list0c = [
+    'apples',  # macintosh
+    'milk',
+    'bread',
+    'milk',  # selyanske
+    'bread',
+    'milk',
+    'bread',
+    'milk',
+    'bread',
+    'bread',
+    'bread',
+    'milk',
+]
+print(my_list0c)
+my_list1 = ['soccer', 'tennis', 'chess']
 
 people = [
     "Гордей",
