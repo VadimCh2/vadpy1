4b2b5c340e436e8c4c5f30af8e75edad969af0d9
“John <john@email.com>
2026-04-25 16:35:28 +0300

    hw10

--- hw10.py
+++ hw10.py
@@ -1,14 +1,38 @@
 import requests
-from pprint import pprint
 
 url = 'https://dummyjson.com/recipes'
 
 response = requests.get(url=url)
 response_json = response.json()
-cuisine = response_json['cuisine']
 
-count = 0
+pizza_count = 0
+cuisine_count = 0
+cooking_190 = []
+total_reviews = 0
 
-for all_cuisines in cuisine:
-    if all_cuisines['cuisine'] == count:
-        count += 1
+
+most_caloric = response_json["recipes"][0]
+
+for recipe in response_json["recipes"]:
+
+    if "pizza" in recipe["name"].lower():
+        pizza_count += 1
+
+    if recipe["cuisine"].lower() == "italian":
+        cuisine_count += 1
+
+    if recipe["caloriesPerServing"] > most_caloric["caloriesPerServing"]:
+        most_caloric = recipe
+
+    if "190" in recipe["instructions"][0]:
+        cooking_190.append(recipe["name"])
+    total_reviews += recipe["reviewCount"]
+
+print("Кількість піц:", pizza_count)
+print("Італійських страв:", cuisine_count)
+print("Найкалорійніша страва:", most_caloric["name"])
+print("Калорії:", most_caloric["caloriesPerServing"])
+print("Всього переглядів:", total_reviews)
+print("Страви при 190°C:")
+for recipe2 in cooking_190:
+    print(recipe2)
