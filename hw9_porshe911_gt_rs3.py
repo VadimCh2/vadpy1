1a48d9277d48f5174698fe3c5e7e31f6f2686a7e
“John <john@email.com>
2026-04-25 17:00:37 +0300

    hw

--- hw9_porshe911_gt_rs3.py
+++ hw9_porshe911_gt_rs3.py
@@ -19,22 +19,25 @@ porshe_911_gt_rs3 = {
     }
 }
 
-# 2. Немає причепа
-car_model = porshe_911_gt_rs3.get("model")
+car_model = porshe_911_gt_rs3["model"]
 print(car_model)
 
-car_price = porshe_911_gt_rs3.get("price")
-print(car_price,"грн")
+car_price = porshe_911_gt_rs3["price"]
+print(car_price, "грн")
 
-first_feature = porshe_911_gt_rs3.get("interior_features")[0]
+features = porshe_911_gt_rs3.get("interior_features", [])
+first_feature = features[0] if features else None
 print(first_feature)
 
-folded_trunk = porshe_911_gt_rs3.get("trunk").get("folded_seats_volume")
-print(folded_trunk,"л")
+trunk = porshe_911_gt_rs3.get("trunk", {})
+folded_trunk = trunk.get("folded_seats_volume")
+print(folded_trunk, "л")
 
 insurance = porshe_911_gt_rs3["price"] * 0.005
 porshe_911_gt_rs3["insurance_payment"] = insurance
-print(insurance,"грн")
+print(insurance, "грн")
 
-trip_cost = 2 * porshe_911_gt_rs3["fuel_consumption"] * 93
+FUEL_PRICE = 93
+
+trip_cost = 2 * porshe_911_gt_rs3["fuel_consumption"] * FUEL_PRICE
 print("Вартість подорожі на 200 км:", trip_cost, "грн")
